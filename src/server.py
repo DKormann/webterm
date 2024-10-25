9a91e2a1ff27d458d87674a37bec2be1eb4ed6cd
DKormann <dominikormann@outlook.com>
2024-10-25 17:19:14 +0200

    autotab path

--- src/server.py
+++ src/server.py
@@ -10,47 +10,61 @@ out_q = None
 
 report_path = '/tmp/report'
 
+sessions = {}
+
 def stream_output(stream, q):
   while True:
     try: q.put(stream.read(1))
     except ValueError: break
 
-@app.route('/run', methods=['POST'])
-def run():
-  shell.stdin.write(request.json['command'] + '\n')
-  shell.stdin.flush()
-  return 'ok'
-
-@app.route('/command', methods=['POST'])
-def command():
-  if os.path.exists(report_path): os.remove(report_path)
-  pipe = os.mkfifo(report_path)
-  shell.stdin.write(f'{request.json["command"]} > {report_path}\n')
-  shell.stdin.flush()
-  with open(report_path) as f: return f.read()
+sess_counter = 0
+def request_session(): return sessions.get(int(request.get_json()['session']), None)
 
 @app.route('/newsession')
 def newsession():
-  global shell, out_q
-  if shell: shell.terminate()
-  shell = subprocess.Popen(['bash'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-  return 'ok'
+  global sess_counter
+  sess_counter += 1
+  sessions[sess_counter] = TerminalSession(sess_counter)
+  return str(sess_counter)
 
-
-@app.route('/subscribe')
-def subscribe():
-  global out_q, threads
-  if out_q: out_q.close()
-  out_q = Queue()
-  threads = [threading.Thread(target=stream_output, args=(pipe, out_q)) for pipe in [shell.stdout, shell.stderr]]
-  for t in threads: t.start()
-  def generate():
-    while True:
-      try: yield out_q.get(timeout=1)
-      except queue.Empty: pass
-      except OSError: break
-  return Response(stream_with_context(generate()))
+class TerminalSession:
+  def __init__(self, id):
+    self.id = str(id)
+    self.shell = subprocess.Popen(['bash'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    self.out_q = None
   
+  def subscribe(self):
+    if self.out_q: self.out_q.close()
+    self.out_q = Queue()
+    self.threads = [threading.Thread(target=stream_output, args=(pipe, self.out_q)) for pipe in [self.shell.stdout, self.shell.stderr]]
+    for t in self.threads: t.start()
+    def generate():
+      while True:
+        try: yield self.out_q.get(timeout=1)
+        except queue.Empty: pass
+        except OSError: break
+    return Response(stream_with_context(generate()), content_type='text/plain')
+  
+  def command(self):
+    if os.path.exists(report_path+self.id): os.remove(report_path+self.id)
+    os.mkfifo(report_path+self.id)
+    self.shell.stdin.write(f'{request.get_json()["command"]} > {report_path+self.id}\n')
+    self.shell.stdin.flush()
+    with open(report_path+self.id) as f: return f.read()
+  
+  def run(self):
+    command = request.get_json()['command']
+    self.shell.stdin.write(command + '\n')
+    self.shell.stdin.flush()
+    return 'ok'
+
+for fname in TerminalSession.__dict__:
+  if fname.startswith('_'): continue
+  def handler():
+    name = fname
+    return lambda : getattr(request_session(), name)()
+  app.route('/'+fname, methods=['POST'], endpoint = '/'+fname)(handler())
+
 app.route('/')(lambda: send_from_directory('static', 'index.html'))
 
 if __name__ == '__main__':
@@ -60,4 +74,3 @@ if __name__ == '__main__':
   else:
     # os.system('open http://localhost:1358')
     app.run(port=1358)
-
